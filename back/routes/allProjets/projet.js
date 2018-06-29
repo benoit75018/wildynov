5658ce74d6697c0112f2ce19999956d053421071
benoit <rameaubenoit@hotmail.fr>
2018-06-29 19:25:24 +0200

    mise ajour route projet

---
+++ back/routes/allProjets/projet.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const router = express.Router();
+const connection = require('../../helpers/connect.js');
+
+
+
+////////////Route pour ajouter un projet sur la page ajout projets ///////////
+router.post('/addProjet', (req, res) => {
+
+    const data = [
+        req.body.title,
+        req.body.deadline_project,
+        req.body.deadline_application,
+        req.body.description,
+        req.body.year
+    ]
+
+    const INSERT_QUERY = `INSERT INTO project ( title, 
+        deadline_project, deadline_application, description) VALUES (?,?,?,?,?)`
+
+    connection.query(INSERT_QUERY, data,
+        (err, results) => {
+            if (err) {
+                res.send(err);
+            } else {
+                return res.send({
+                    results
+                })
+            }
+
+        })
+})
+module.exports = router;
--- back/routes/allprojets/projets.js
+++ back/routes/allprojets/projets.js
@@ -1,21 +1,11 @@
 const express = require('express')
 const router = express.Router()
-<<<<<<< HEAD:back/routes/allProjets/projets.js
-const verifToken = require('../../middleware/verifToken.js')
-=======
->>>>>>> 7e16eac74f3c60ed2cefad5961b2659d5ba2ee2c:back/routes/allprojets/projets.js
 const connection = require('../../helpers/connect.js')
 
 
 
-<<<<<<< HEAD:back/routes/allProjets/projets.js
-
-/// ////////Afficher un projet ////////////////////////////
-router.get('/showProjet', verifToken, (req, res) => {
-=======
 ///////////Route pour afficher les projets sur la page projet////////////////////////////
 router.get('/showProjet', (req, res) => {
->>>>>>> 7e16eac74f3c60ed2cefad5961b2659d5ba2ee2c:back/routes/allprojets/projets.js
   const SELECT_QUERY = 'SELECT * FROM project'
   connection.query(SELECT_QUERY, (err, results) => {
     if (err) {
