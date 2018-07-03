cd981920345ecb230e954a1a719742624e08135f
Mohand H <mohandhassam1@gmail.com>
2018-07-03 16:13:28 +0200

    modif code route projet

--- back/routes/allprojets/projet.js
+++ back/routes/allprojets/projet.js
@@ -1,35 +1,34 @@
 const express = require('express');
 const router = express.Router();
 const connection = require('../../helpers/connect.js');
+const verifToken = require('../../middleware/verifToken.js')
 
 
 
 ////////////Route pour ajouter un projet sur la page ajout projets ///////////
-router.post('/addProjet', (req, res) => {
+router.post('/addProjet', verifToken, (req, res) => {
 
-    const data = [
-
+    connection.query(`
+    INSERT INTO project
+    SET title=?,
+        profile_id=?,
+        deadline_project=?,
+        deadline_application=?,
+        description=?`, [
         req.body.title,
+        req.token.userID,
         req.body.deadline_project,
         req.body.deadline_application,
         req.body.description,
-        req.body.profile_id
-    ]
+    ], (err, results) => {
+        if (err) {
+            res.send(err);
+        } else {
+            return res.send({
+                results
+            })
+        }
 
-
-    const INSERT_QUERY = `INSERT INTO project ( title, 
-        deadline_project, deadline_application, description, profile_id) VALUES (?,?,?,?,?)`//
-
-    connection.query(INSERT_QUERY, data,
-        (err, results) => {
-            if (err) {
-                res.send(err);
-            } else {
-                return res.send({
-                    results
-                })
-            }
-
-        })
+    })
 })
 module.exports = router;
