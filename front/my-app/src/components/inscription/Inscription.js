eef0932ce648df5d051c6e862d01f82749bc95c9
Souhail <mr.souid@live.fr>
2018-07-05 17:27:54 +0200

    deleted selectcampus...

--- front/my-app/src/components/inscription/Inscription.js
+++ front/my-app/src/components/inscription/Inscription.js
@@ -1,13 +1,5 @@
 import React from 'react'
-// Menus déroulants
-<<<<<<< HEAD
-// import SelectCampus from '../profil/SelectCampus'
-// import SelectPromo from '../profil/SelectPromo'
 
-=======
-import SelectCampus from '../profil/SelectCampus'
-import SelectPromo from '../profil/SelectPromo'
->>>>>>> 5c93ebed11db8ac363477d1acb97569c9acf6d4c
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles'
 import Button from '@material-ui/core/Button'
 import TextField from 'material-ui/TextField'
@@ -41,7 +33,6 @@ class Inscription extends React.Component {
 				</a>
 				<NavBar />
 
-<<<<<<< HEAD
 				<MuiThemeProvider theme={theme}>
 					<nav>
 						<TextField hintText="Nom" floatingLabelText="Nom" />
@@ -63,34 +54,6 @@ class Inscription extends React.Component {
 		)
 	}
 }
-=======
-	<div>
-		<a linkButton={true} href="/home" class="brand-logo center" alt="">
-			{<img src="https://image.noelshack.com/fichiers/2018/24/3/1528883121-webp-net-resizeimage.png" alt=""/>}
-		</a>
-		<NavBar />
-
-		<MuiThemeProvider theme={theme}>
-			<nav>
-				<TextField hintText="Nom" floatingLabelText="Nom" />
-				<br />
-				<TextField hintText="Prénom" floatingLabelText="Prénom" />
-				<br />
-                <TextField hintText="Compétences" floatingLabelText="Compétences" />
-                <br />
-				<SelectCampus />
-				<br />
-				<SelectPromo />
-				<br />
-				<Button variant="raised" color="primary" style={style}>
-					Enregistrer
-				</Button>
-			</nav>
-		</MuiThemeProvider>
-	</div>
-)
-	}}
->>>>>>> 5c93ebed11db8ac363477d1acb97569c9acf6d4c
 const style = {
 	margin: 15
 }
